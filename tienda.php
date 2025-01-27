71684957dbfa7349ef0e6388e0e0fec91d84a3db
tania1985 <testevezbautista@danielcastelao.org>
2025-01-27 20:25:45 +0100

    mejorando

--- tienda.php
+++ tienda.php
@@ -7,8 +7,10 @@ if (isset($_SESSION["username"])) {
     exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,20 +18,33 @@ if (isset($_SESSION["username"])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="CSS/tienda.css">
+    <link rel="stylesheet" href="css/tienda.css">
 </head>
-<body>
-    <header>
-        <i class="fa-solid fa-store"></i>
-        <p><?php echo $_SESSION["username"]; ?></p>
-    </header>
-    <main>
-        <aside></aside>
-        <section>
 
-        </section>
-    </main>
-    <footer></footer>
+<body>
+    <div class="container">
+        <header>
+            <i class="fa-solid fa-store"></i>
+            <p><?php echo $_SESSION["username"]; ?></p>
+        </header>
+        <main>
+            <aside>
+                <ul >
+                    <li><a href="tienda"><i class="fa-solid fa-store"></i>Inicio</a></li>
+                    <li><a href="proveedores"><i class="fa-solid fa-boxes-packing"></i>Proveedores</a></li>
+                    <li><a href="clientes"><i class="fa-solid fa-user"></i>Clientes</a></li>
+                    <li><a href="categorias"><i class="fa-solid fa-list"></i>Categoriass</a></li>
+                    <li><a href="productos"><i class="fa-brands fa-product-hunt"></i>Productos</a></li>
+                    <li><a href="ventas"><i class="fa-brands fa-salesforce"></i>Ventas</a></li>
+                </ul>
+            </aside>
+            <section>
+
+            </section>
+        </main>
+        <footer></footer>
+    </div>
+
 </body>
 
 </html>
