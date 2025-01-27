3008100ee77ef6d0ba10f63d58ef348d024b8ec6
tania1985 <testevezbautista@danielcastelao.org>
2025-01-27 17:19:59 +0100

    creando ficheros de documentación

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define(constant_name: "HOST", value: "localhost");
+define(constant_name:"DB_USER", value:"tienda");
+define(constant_name:"DB_PASS", value:"1234");
+define(constant_name:"DB_DATABASE", value:"tienda");
+?>
