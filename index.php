e902510952ac90d18515876d9a37b36f3fe15e65
tania1985 <testevezbautista@danielcastelao.org>
2025-01-27 16:47:43 +0100

    creando la aplicación de tienda

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link rel="stylesheet" href="CSS/index.css">
+</head>
+<body>
+    <form action="" method="post">
+        <label for="username">Nombre de usuario</label>
+        <input type="text" name="username" id="username" required placeholder="Introduce tu nombre de usuario">
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" required placeholder="Introduce tu contraseña">
+        <input type="submit" value="Iniciar sesión">
+    </form>
+</body>
+</html>
