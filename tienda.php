0f03f7efbee5141b64f5399af739a06712785454
tania1985 <testevezbautista@danielcastelao.org>
2025-01-27 20:06:21 +0100

    css de tienda

---
+++ tienda.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+} else {
+    header("Location: index");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="CSS/tienda.css">
+</head>
+<body>
+    <header>
+        <i class="fa-solid fa-store"></i>
+        <p><?php echo $_SESSION["username"]; ?></p>
+    </header>
+    <main>
+        <aside></aside>
+        <section>
+
+        </section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
