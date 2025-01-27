6df1c235c68992b34b0050c0dd8a90246d490981
tania1985 <testevezbautista@danielcastelao.org>
2025-01-27 19:22:53 +0100

    mejorando el css

--- conexiondb.php
+++ conexiondb.php
@@ -1,16 +1,13 @@
 <?php
-$host = "localhost"; // o la IP de tu servidor MySQL
-$user = "tienda";
-$password = "1234";
-$database = "tienda";
-try {
+include("config.php");
+$host = HOST; // o la IP de tu servidor MySQL
+$user = DB_USER;
+$password = DB_PASS;
+$database = DB_DATABASE;
+
     // Crear la conexión con PDO
     $conexion = new PDO("mysql:host=$host;dbname=$database", $user, $password);
     // Establecer el modo de error de PDO a excepción
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Conexión exitosa";
-} catch (PDOException $e) {
-    echo "Conexión fallida: " . $e->getMessage();
-}
-
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php
 if (isset($_POST["username"])) {
     include("conexiondb.php");
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $sql = "SELECT * FROM usuarios WHERE username = :username";
-    $stm=$conexion->prepare($sql);
-    $stm->bindParam(":username", $username);
-    $stm->execute();
-    $row=$stm->fetch(PDO::FETCH_ASSOC);
-    if($row) {
-        var_dump($row);
-        exit();
-    } else {
-        var_dump($row);
-        exit();
+    try{
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $sql = "SELECT * FROM usuarios WHERE username = :username";
+        $stm=$conexion->prepare($sql);
+        $stm->bindParam(":username", $username);
+        $stm->execute();
+        $row=$stm->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            if(password_verify($password, $row["password"])) {
+                session_start();
+                $_SESSION["username"] = $username;
+                header("Location: tienda.php");
+            } else {
+                $error = "Usuario o contraseña incorrectos";
+            }
+        } else {
+            $error = "Usuario o contraseña incorrectos";
+        }  
+    }catch(Exception $e){
+        $error = "Error al iniciar sesión";
     }
 }
 ?>
@@ -27,11 +35,16 @@ if (isset($_POST["username"])) {
 </head>
 <body>
     <form action="" method="post">
+        <h1>Iniciar sesión tienda</h1>
         <label for="username">Nombre de usuario</label>
         <input type="text" name="username" id="username" required placeholder="Introduce tu nombre de usuario">
         <label for="password">Contraseña</label>
         <input type="password" name="password" id="password" required placeholder="Introduce tu contraseña">
         <input type="submit" value="Iniciar sesión">
+        <?php if (isset($error)) {
+            echo "<p>$error</p>";
+        } 
+        ?>
     </form>
 </body>
 </html>
